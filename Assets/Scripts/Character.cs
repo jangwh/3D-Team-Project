f28e18c4e9d750484e77eacfae2c769ea98ca2a9
장우형 <janghy95@naver.com>
2025-08-05 09:20:16 +0900

    플레이어 이동
    1. 플레이어의 간단한 이동 추가

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -7,7 +7,7 @@ public class Character : MonoBehaviour
     public float maxHp;
     public float currentHp;
     public float damage;
-
+    public float moveSpeed;
     public float lastAttackTime;
     public float attackInterval;
 
